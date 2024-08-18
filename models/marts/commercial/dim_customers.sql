98e271a5a32555071f7fc4a4d0126e3d504faa87
Gabriela Klitzke <g.amklitzke@gmail.com>
2024-08-18 18:41:58 +0000

    creating rfm categorization

--- models/marts/commercial/dim_customers.sql
+++ models/marts/commercial/dim_customers.sql
@@ -11,6 +11,10 @@ with
         select *
         from {{ ref('stg_sap__customers') }}
     )
+    , rfm as (
+        select *
+        from {{ ref('int_commercial__rfm_analysis') }}
+    )
     , joined as (
         select
             customers.pk_customer
@@ -18,24 +22,32 @@ with
             , customers.fk_store
             , stores.store_name
             , people.person_name
+            , rfm.rfm_segment
         from customers
         left join stores
             on customers.fk_store= stores.pk_store
         left join people
             on customers.fk_person = people.pk_people
+        left join rfm
+            on customers.pk_customer = rfm.fk_customer
     )
-select
-    pk_customer
-    , case 
-        when joined.fk_store is null then joined.person_name
-        when joined.fk_store is not null then joined.store_name 
-        else joined.person_name
-    end as customer_name --joining person and store name in one column.
-    , case 
-        when fk_store is null then 'Person'
-        when fk_store is not null then 'Store'
-    end as customer_type
-    /*Since there is two types of customers, stores and people then I created a new columns with customer name.
-    There are some stores that have also a person associated with it. In this cases I got the person name as the
-    customer name, as the store name was already considered when the person fk is null.*/
-from joined
+    , categorization as (
+        select
+            pk_customer
+            , case 
+                when joined.fk_store is null then joined.person_name
+                when joined.fk_store is not null then joined.store_name 
+                else joined.person_name
+            end as customer_name --joining person and store name in one column.
+            , case 
+                when fk_store is null then 'Person'
+                when fk_store is not null then 'Store'
+            end as customer_type
+            /*Since there is two types of customers, stores and people then I created a new columns with customer name.
+            There are some stores that have also a person associated with it. In this cases I got the person name as the
+            customer name, as the store name was already considered when the person fk is null.*/
+            , rfm_segment
+        from joined
+    )
+select *
+from categorization
